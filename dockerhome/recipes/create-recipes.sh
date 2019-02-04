2f8f7734ab9651326eefabe77d5e408cf7086908
xy124 <qcd@gmx.net>
2019-02-04 11:22:44 +0100

    Add git revision for netcdf-c

--- dockerhome/recipes/create-recipes.sh
+++ dockerhome/recipes/create-recipes.sh
@@ -7,3 +7,14 @@ cp -f ../flowvr-ex/build/CMakeCache.txt cmake/flowvr-ex/
 
 mkdir -p cmake/parflow
 cp -f ../parflow/build/CMakeCache.txt cmake/parflow/
+
+T="$PWD/git-revisions"
+echo ParFlowVR: > $T
+cd ../parflow
+git rev-parse HEAD >> $T
+echo FlowVR: >> $T
+cd ../flowvr-ex
+git rev-parse HEAD >> $T
+cd ../deps/netcdf-c
+echo netcdf-c: >> $T
+git rev-parse HEAD >> $T
